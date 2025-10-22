5adefc8a01d914eccaf3898d87d7e39ed0a19553
Paul_K <p.keptene@gmail.com>
2025-10-22 15:19:09 +0300

    Fixed Solution

--- app/main.py
+++ app/main.py
@@ -8,24 +8,8 @@ def cache(func: Callable) -> Callable:
     @wraps(func)
     def inner(*args, **kwargs) -> Callable:
         cache_key = (func, args, tuple(kwargs.items()))
-        mutable_flag = False
 
-        if not mutable_flag:
-            for arguments in args:
-                if type(arguments) in (dict, list, set):
-                    mutable_flag = True
-                    break
-
-        if not mutable_flag:
-            for key, value in kwargs.items():
-                if type(value) in (dict, list, set):
-                    mutable_flag = True
-                    break
-
-        if mutable_flag:
-            print("Arguments must be immutable")
-            return func(*args, **kwargs)
-        elif cache_key in cache_immutable_values:
+        if cache_key in cache_immutable_values:
             print("Getting from cache")
             return cache_immutable_values[cache_key]
         else:
