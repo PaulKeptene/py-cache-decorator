41a369248ad1baf8f9ad3bdcc3c95eeaefc05d8f
Paul_K <p.keptene@gmail.com>
2025-10-22 15:14:28 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,36 @@
+from functools import wraps
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_immutable_values = {}
+
+    @wraps(func)
+    def inner(*args, **kwargs) -> Callable:
+        cache_key = (func, args, tuple(kwargs.items()))
+        mutable_flag = False
+
+        if not mutable_flag:
+            for arguments in args:
+                if type(arguments) in (dict, list, set):
+                    mutable_flag = True
+                    break
+
+        if not mutable_flag:
+            for key, value in kwargs.items():
+                if type(value) in (dict, list, set):
+                    mutable_flag = True
+                    break
+
+        if mutable_flag:
+            print("Arguments must be immutable")
+            return func(*args, **kwargs)
+        elif cache_key in cache_immutable_values:
+            print("Getting from cache")
+            return cache_immutable_values[cache_key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_immutable_values[cache_key] = result
+            return result
+    return inner
